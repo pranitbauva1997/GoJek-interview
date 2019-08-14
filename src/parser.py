16217acdf8bc96f196d6c5740b7df44b93c427c0
Pranit Bauva <pranit.bauva@gmail.com>
2019-08-15 02:56:53 +0530

    create a command line parser to interpret commands
    A simple command line parser to interpret commands and then do various
    library calls and display the result.
    
    This is the final binary which is to be produced who's path is to be
    put in the `bin/` directory.
    
    More rigrous testing, documentation required.
    
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

---
+++ src/parser.py
@@ -0,0 +1,36 @@
+import sys
+
+from parking_lot import ParkingLot
+from car         import Car
+
+pk = None
+
+line_str = raw_input('')
+
+try:
+    while line_str != None:
+        line = line_str.split(' ')
+
+        if   line[0] == 'create_parking_lot':
+            pk = ParkingLot(int(line[1]))
+        elif line[0] == 'park':
+            Car.create_and_park(pk, line[1], line[2])
+        elif line[0] == 'leave':
+            pk.eject(int(line[1]) - 1)
+        elif line[0] == 'status':
+            pk.status()
+        elif line[0] == 'registration_numbers_for_cars_with_colour':
+            arr = pk.registration_nos_for_cars_with_colour(line[1])
+            print ', '.join(arr)
+        elif line[0] == 'slot_numbers_for_cars_with_colour':
+            arr = pk.slots_for_cars_with_colour(line[1])
+            print ', '.join(arr)
+        elif line[0] == 'slot_number_for_registration_number':
+            print pk.slot_for_registration_number(line[1])
+
+        line_str = raw_input('')
+
+except EOFError:
+    sys.exit(0)
+except Exception:
+    print e
