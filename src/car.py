61842b430502468b577b2d12bfd791426787cd59
Pranit Bauva <pranit.bauva@gmail.com>
2019-08-16 01:13:58 +0530

    add documentation
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- src/car.py
+++ src/car.py
@@ -1,11 +1,58 @@
 from custom_exceptions import *
 
 class Car:
+    """A class used to represent the Car object
+
+    This car object can be parked inside a ParkingLot. An attribute named
+    `registration_no` is the unique property of the car.
+
+    Attributes
+    ----------
+    colour          : str
+        Major colour of the car used for identification
+    registration_no : str
+        Unique code issued to the car by the government
+    slot            : str
+        A spot in the parking lot
+
+    Methods
+    -------
+    create_and_park(parking_lot, registration_no, colour)
+        Creates a car object and parks it in the `parking_lot`
+    park(parking_lot)
+        Allocates a slot for the car in the parking lot (Do not use directly)
+    """
+
     colour = None
     registration_no = None
     slot = None
 
     def __init__(self, registration_no, colour):
+        """Default constructor
+
+        Do not use directly. Instead call the `create_and_park()` method
+        which initializes a new object and calls `park()` and also does
+        exception handling.
+
+        Parameters
+        ----------
+        registration_no : str
+            Unique code issued to the car by the government
+        color           : str
+            Major colour of the car used for identification
+
+        Returns
+        -------
+        message : str
+            status message to be echoed in STDOUT
+
+        Raises
+        -------
+        CarInvalidInputs
+            If invalid inputs like no registration number or no colour
+            are specified.
+        """
+
         if registration_no is None or colour is None:
             raise CarInvalidInputs
 
@@ -13,6 +60,33 @@ class Car:
         self.colour = colour
 
     def park(self, parking_lot):
+        """Parks the car in the `parking_lot`
+
+        Do not use this directly. Instead prefer `create_and_park()`.
+
+        Parameters
+        ----------
+        parking_lot : ParkingLot
+            The `ParkingLot` object to which we want to add our car
+
+        Returns
+        ----------
+        message : str
+            status message to be echoed in STDOUT
+
+        Raises
+        ----------
+        ParkingLotUnitialized
+            If there are issues in creating a new parking not or trying
+            to access a non-existent parking lot.
+        ParkingLotFull
+            If there is no space left in the parking lot for an extra
+            car.
+        ParkingSlotEmpty
+            If for some serious reason `vehicles` attribute of parking_lot
+            doesn't exist.
+        """
+
         if len(parking_lot.vehicles) < 1:
             raise ParkingLotUninitialized('Please initialize parking lot')
 
@@ -25,6 +99,35 @@ class Car:
 
     @staticmethod
     def create_and_park(parking_lot, registration_no, colour):
+        """Create and park a car
+
+        This the recommended way of creating a new car and parking it
+        in the parking lot.
+
+        Parameters
+        ----------
+        parking_lot : ParkingLot
+            The `ParkingLot` object to which we want to add our car
+        registration_no : str
+            Unique code issued to the car by the government
+        color           : str
+            Major colour of the car used for identification
+
+        Returns
+        ----------
+        car     : Car
+            The newly created car which is parked
+        message : str
+            status message to be echoed in STDOUT
+
+        Raises
+        ----------
+        ParkingLotUninitialized
+            If you try to add a car to a non-existent parking lot.
+        ParkingLotFull
+            If no slots available to park the new car.
+        """
+
         if parking_lot is None or parking_lot.vehicles is None:
             raise ParkingLotUninitialized('Please initialize parking lot')
 
@@ -40,5 +143,7 @@ class Car:
             return None, e
 
     def __str__(self):
+        """Readable string for debugging"""
+
         return '{0}. {1} {2}'.format(self.slot, self.registration_no,
                                      self.colour)
--- src/parking_lot.py
+++ src/parking_lot.py
@@ -1,11 +1,41 @@
 from custom_exceptions import *
 
 class ParkingLot:
+    """A class used to represent the Parking Lot object
+
+    This parking lot object can have many parked cars inside it.
+
+    Attributes
+    ----------
+    vehicles : [str]
+        Array of car objects parked in their respective slots
+
+    Methods
+    ----------
+    add(car)
+        Add a car to the parking lot
+    get_first_empty()
+        Get the nearest empty slot to park a new car
+    leave(slot)
+        Let go of the car from the parking spot
+    status()
+        Show the slots, registration numbers and colour of the cars
+        currently parked in the parking lot.
+    registration_numbers_for_cars_with_colour(colour)
+        Show the registration numbers of cars who's colour matches the
+        parameter.
+    slot_numbers_for_cars_with_colour(colour)
+        Show the slot numbers of cars who's colour matches the parameter.
+    slot_number_for_car_with_registration_number(registration_no)
+        Show the slot number of the car with the given registration number.
+    """
+
     # Remember to +1 the index of vehicles while displaying the data
     # and -1 the index of vehicles while storing the data
     vehicles = None
 
     def __init__(self, size):
+        """Default Constructor"""
         if (size is None) or (not isinstance(size, int)) or (size < 1):
             raise ParkingLotInvalidInputs
 
@@ -13,6 +43,29 @@ class ParkingLot:
         print('Created a parking lot with {} slots'.format(size))
 
     def add(self, car):
+        """Add a car object to the parking lot
+
+        This method is internally called by `create_and_park()` and should
+        be avoided being called directly.
+
+        Parameters
+        ----------
+        car : Car
+            The car object to be parked
+
+        Returns
+        ----------
+        slot   : int
+            The slot allocated after parking
+        message : str
+            status message to be echoed in STDOUT
+
+        Raises
+        ----------
+        ParkingLotFull
+            If there is no space left in the parking lot for an extra car.
+        """
+
         slot = self.get_first_empty()
         if slot > len(self.vehicles) - 1:
             raise ParkingLotFull('Sorry, parking lot is full')
@@ -21,10 +74,24 @@ class ParkingLot:
         return slot,  'Allocated slot number: {}'.format(slot + 1)
 
     def get_first_empty(self):
+        """Get the nearest empty slot to park a new car
+
+        Internal method. Not be called from outside.
+
+        Returns
+        ----------
+        slot : int
+            the next free slot available for parking
+
+        Raises
+        ----------
+        ParkingLotUninitialized
+        """
+
         i = 0
 
         if self is None or self.vehicles is None:
-            raise ParkingLotUninitialized('Please initialize parking lot')
+            raise ParkingLotUninitialized
 
         for car in self.vehicles:
             if car is None:
@@ -35,6 +102,23 @@ class ParkingLot:
         return i
 
     def leave(self, slot):
+        """Let go of the car from the parking spot
+
+        The car object is now discarded and the spot on parking lot is
+        marked empty for the new car.
+
+        Returns
+        ----------
+        message : str
+            status message to be echoed in STDOUT
+
+        Raises
+        ----------
+        ParkingSlotEmpty
+            If for some serious reason `vehicles` attribute of parking lot
+            doesn't exist.
+        """
+
         if self.vehicles[slot] is None:
             raise ParkingSlotEmpty
 
@@ -44,6 +128,8 @@ class ParkingLot:
         return 'Slot number ' + str(slot + 1) + ' is free'
 
     def status(self):
+        """Show the summary of the parking lot with car details"""
+
         messages = []
         messages.append('{0:<10} {1:<20} {2:<10}'.format('Slot No.',
                                                          'Registration No',
@@ -60,6 +146,8 @@ class ParkingLot:
         return messages
 
     def registration_numbers_for_cars_with_colour(self, colour):
+        """Show registration numbers for cars of the specified colour"""
+
         output = []
         for car in self.vehicles:
             if car.colour == colour:
@@ -68,6 +156,7 @@ class ParkingLot:
         return output
 
     def slot_numbers_for_cars_with_colour(self, colour):
+        """Show the slot numbers for cars of the specified colour"""
         output = []
         for car in self.vehicles:
             if car.colour == colour:
@@ -76,6 +165,7 @@ class ParkingLot:
         return output
 
     def slot_number_for_registration_number(self, registration_no):
+        """Show the slot number for car with a registration number"""
         for car in self.vehicles:
             if car.registration_no == registration_no:
                 return str(car.slot + 1)
@@ -83,4 +173,5 @@ class ParkingLot:
         return 'Not found'
 
     def __str__(self):
+        """Readable string for debugging"""
         return ', '.join(self.vehicles)
